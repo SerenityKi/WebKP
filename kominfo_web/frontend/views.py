34c3e3bafb27a507bc08b3b6d3f394158387bcd2
SerenityKi <143372766+SerenityKi@users.noreply.github.com>
2025-01-01 13:31:29 +0700

    Delete kominfo_web directory

--- kominfo_web/frontend/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'frontend/index.html')
-
-def profil(request):
-    return render(request, 'frontend/profil.html')
-
-# Tambahkan views untuk halaman lain...
